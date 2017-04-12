693f5515ae547f62a10fc2c419d3e7a6ef183291
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2017-04-12 19:34:57 +0700

    - Delete ignored items

--- Python/RevertAndCleanUp.py
+++ Python/RevertAndCleanUp.py
@@ -3,28 +3,6 @@ import os, shutil
 from subprocess import Popen, PIPE
 
 
-# Get external links
-def GetExternals(path) :
-    print("Getting external links of " + path)
-
-    process = Popen(["svn", "status", path], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-
-    if len(stderr) > 0 :
-        print("Error: " + stderr)
-        return
-
-    externals = []
-    notation = "X       "
-    lines = stdout.splitlines()
-    for line in lines :
-        if line.startswith(notation) :
-            line = line[len(notation):]
-            externals.append(line)
-
-    return externals
-
-
 # Revert target folder only
 def Revert(path) :
     print("Reverting folder " + path)
@@ -36,36 +14,47 @@ def Revert(path) :
         print("Error: " + stderr)
 
 
-# Get unversioned files
-def GetUnversionedFiles(path) :
-    print("Getting unversioned files...")
-
-    process = Popen(["svn", "status", path], stdout=PIPE, stderr=PIPE)
+def GetItemsByFilter(path, filter) :
+    process = Popen(["svn", "status", "--no-ignore", path], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
 
     if len(stderr) > 0 :
         print("Error: " + stderr)
         return
 
-    fileList = []
-    notation = "?       "
+    itemList = []
     lines = stdout.splitlines()
     for line in lines :
-        if line.startswith(notation) :
-            line = line[len(notation):]
+        if line.startswith(filter) :
+            line = line[len(filter):]
             line = os.path.abspath(line)
-            fileList.append(line)
+            itemList.append(line)
 
-    return fileList
+    return itemList
 
 
-# Delete unversioned files
-def DeleteUnversionedFiles(filepath) :
-    print("Deleting " + filepath)
-    if os.path.isfile(filepath):
-        os.remove(filepath)  # remove the file
-    elif os.path.isdir(filepath):
-        shutil.rmtree(filepath)  # remove dir and all contains
+def GetExternals(path) :
+    print("Getting external links of " + path)
+    return GetItemsByFilter(path, "X       ")
+
+
+def GetUnversionedItems(path) :
+    print("Getting unversioned items...")
+    return GetItemsByFilter(path, "?       ")
+    
+
+def GetIgnoredItems(path) :
+    print("Getting ignored items...")
+    return GetItemsByFilter(path, "I       ")
+
+
+# Delete files or folder
+def DeleteItem(path) :
+    print("Deleting " + path)
+    if os.path.isfile(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
 
 
 def main(argv):
@@ -96,11 +85,13 @@ def main(argv):
         Revert(link)
     Revert(fullPath)
 
-    unversionedFiles = GetUnversionedFiles(path)
-    for filepath in unversionedFiles :
-        DeleteUnversionedFiles(filepath)
+    unversionedItems = GetUnversionedItems(path)
+    for item in unversionedItems :
+        DeleteItem(item)
 
-
+    ignoredItems = GetIgnoredItems(path)
+    for item in ignoredItems :
+        DeleteItem(item)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
