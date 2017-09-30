be82b4410618559b618fa421a273f0775de43946
Unknown <thanhdungspk@yahoo.com.vn>
2017-09-30 21:41:19 +0700

    - Redefine Equipment class

--- Python/IdleHeroes/objects/equipments.py
+++ Python/IdleHeroes/objects/equipments.py
@@ -2,43 +2,60 @@ import json
 
 class Equipment:
     
-    def __init__(self, color, star, left, top, bottom, right):
+    def __init__(self, color, star, position):
         self.color = color
         self.star = star
-        self.left = left
-        self.top = top
-        self.bottom = bottom
-        self.right = right
+        self.position = position
 
 
 class EquipmentList:
     
+    
     def __init__(self, fileName):
         self.equipments = self.ParseFromFile(fileName)
+    
     
     def ParseFromFile(self, fileName):
         with open(fileName) as fileData:
             jsonData = json.load(fileData)
         equipments = []
+        
         for equipJson in jsonData["equipments"]:
-            equipObj = Equipment(equipJson["color"], equipJson["star"], equipJson["left"], equipJson["top"], equipJson["bottom"], equipJson["right"])
-            equipments.append(equipObj)
+            
+            count = equipJson["left"]
+            while count > 0:
+                equipObj = Equipment(equipJson["color"], equipJson["star"], "left")
+                equipments.append(equipObj)
+                count -= 1
+            
+            count = equipJson["top"]
+            while count > 0:
+                equipObj = Equipment(equipJson["color"], equipJson["star"], "top");
+                equipments.append(equipObj)
+                count -= 1
+            
+            count = equipJson["bottom"]
+            while count > 0:
+                equipObj = Equipment(equipJson["color"], equipJson["star"], "bottom");
+                equipments.append(equipObj)
+                count -= 1
+            
+            count = equipJson["right"]
+            while count > 0:
+                equipObj = Equipment(equipJson["color"], equipJson["star"], "right");
+                equipments.append(equipObj)
+                count -= 1
+        
         return equipments
+    
     
     def ToString(self):
         output = "Equipment List:\n"
         for equipment in self.equipments:
-            output += "* " + equipment.color + "-" + str(equipment.star) + " : "
-            output += str(equipment.left) + " " + str(equipment.top) + " "
-            output += str(equipment.bottom) + " " + str(equipment.right) + "\n"
+            output += "* " + equipment.color + "-" + str(equipment.star) + " " + equipment.position + "\n"
         output += "Total: " + str(self.Count())
         return output
     
+    
     def Count(self):
-        total = 0
-        for equipment in self.equipments:
-            total += equipment.left
-            total += equipment.top
-            total += equipment.bottom
-            total += equipment.right
-        return total
+        return len(self.equipments)
