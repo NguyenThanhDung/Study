07b383520f7a41f144557700b86091b8a870950a
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-14 00:01:57 +0700

    + Learn create custom inspector

---
+++ Unity3D/CustomInspector/Assets/Scripts/Editor/Inspector1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameScript1))]
+public class Inspector1 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameScript1 gsTarget = (GameScript1)target;
+        gsTarget.anInt = EditorGUILayout.IntSlider("AnInt", gsTarget.anInt, 10, 20);
+    }
+}
---
+++ Unity3D/CustomInspector/Assets/Scripts/GameScript1.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript1 : MonoBehaviour
+{
+    public int anInt;
+}
