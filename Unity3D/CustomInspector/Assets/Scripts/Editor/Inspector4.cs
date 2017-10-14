fdef809fc730fc03ed2f83b2bb443a88f6d1e368
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-14 22:00:40 +0700

    + Add Inspector to edit ScriptableObject

---
+++ Unity3D/CustomInspector/Assets/Scripts/Editor/Inspector4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameScript4))]
+public class Inspector4 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameScript4 gameScript = (GameScript4)target;
+        gameScript.speed = EditorGUILayout.IntSlider("Speed", gameScript.speed, 1, 10);
+
+        if(GUILayout.Button("Save"))
+        {
+            EditorUtility.SetDirty(gameScript);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
