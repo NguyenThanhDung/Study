de016b634ecf797e480749d9164ecb06c2c17ba8
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-14 11:30:27 +0700

    + Add list of objects

---
+++ Unity3D/CustomInspector/Assets/Scripts/Editor/Inspector3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameScript3))]
+public class Inspector3 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameScript3 gameScript = (GameScript3)target;
+
+        for(int i=0;i<gameScript.waves.Count;i++)
+        {
+            gameScript.waves[i].speed = EditorGUILayout.IntSlider("Speed", gameScript.waves[i].speed, 1, 20);
+            if(GUILayout.Button("-"))
+            {
+                gameScript.waves.RemoveAt(i);
+            }
+        }
+
+        if (GUILayout.Button("Add"))
+        {
+            gameScript.waves.Add(new GameScript3.Wave());
+        }
+    }
+}
---
+++ Unity3D/CustomInspector/Assets/Scripts/GameScript3.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript3 : MonoBehaviour
+{
+    public class Wave
+    {
+        public int speed;
+    }
+
+    public List<Wave> waves = new List<Wave>(0);
+}
