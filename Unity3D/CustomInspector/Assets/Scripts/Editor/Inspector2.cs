6f32a213f9d9c3cd160d19ca0794b0d540999e9d
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-14 11:18:16 +0700

    + Add list of int

---
+++ Unity3D/CustomInspector/Assets/Scripts/Editor/Inspector2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameScript2))]
+public class Inspector2 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameScript2 gameScript = (GameScript2)target;
+
+        for(int i=0;i<gameScript.listOfInt.Count;i++)
+        {
+            gameScript.listOfInt[i] = EditorGUILayout.IntSlider("Int Value", gameScript.listOfInt[i], 1, 20);
+            if(GUILayout.Button("-"))
+            {
+                gameScript.Remove(i);
+            }
+        }
+
+        if(GUILayout.Button("Add"))
+        {
+            gameScript.AddNew();
+        }
+    }
+}
---
+++ Unity3D/CustomInspector/Assets/Scripts/GameScript2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript2 : MonoBehaviour
+{
+    public List<int> listOfInt = new List<int>(0);
+
+    public void AddNew()
+    {
+        listOfInt.Add(1);
+    }
+
+    public void Remove(int index)
+    {
+        listOfInt.RemoveAt(index);
+    }
+}
