f4089d738948c4e21d713677c070697d3802aab4
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2018-04-10 18:41:38 +0700

    + Add DeviceID and resolution 800x600

--- Python/ChainStrike/AutoTouch.py
+++ Python/ChainStrike/AutoTouch.py
@@ -2,9 +2,12 @@ import sys
 import time
 from subprocess import Popen, PIPE
 
+DEVICE_COMPANY_NOX = 0
+DEVICE_E300S_GL02221_SE = 1
 
-SCREEN_960x540 = 0
-SCREEN_1920x1080 = 1
+SCREEN_800x600 = 0
+SCREEN_960x540 = 1
+SCREEN_1920x1080 = 2
 
 
 def ExecuteCommand(params):
@@ -36,16 +39,25 @@ def TouchPoint(deviceID, point):
     return TouchXY(deviceID, point[0], point[1])
 
 
+def GetDeviceID(device):
+    if device == DEVICE_COMPANY_NOX:
+        return "127.0.0.1:62001"
+    elif device == DEVICE_E300S_GL02221_SE:
+        return "4300650c63ca30e5"
+
+
 def GetReplayButtonPosition(screen):
-    if screen == SCREEN_960x540:
-        return [220, 900]
+    if screen == SCREEN_800x600:
+        return [343, 750]
+    elif screen == SCREEN_960x540:
+        return [445, 1800]
     elif screen == SCREEN_1920x1080:
         return [445, 1800]
 
 
 def main(argv):
-    deviceID = "4300650c63ca30e5"
-    replayButton = GetReplayButtonPosition(SCREEN_1920x1080)
+    deviceID = GetDeviceID(DEVICE_COMPANY_NOX)
+    replayButton = GetReplayButtonPosition(SCREEN_800x600)
     maxTime = 60 * 60
     interval = 5
 
