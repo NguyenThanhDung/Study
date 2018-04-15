2c89607686d0c527ed32006c8b9b42ed153a5d3d
Unknown <thanhdungspk@yahoo.com.vn>
2018-04-15 17:24:55 +0700

    + Add main character

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // VARIABLES
+    [SerializeField]
+    MainCharacter m_mainCharacter;
 
     // PROPERTIES
 
@@ -22,22 +24,34 @@ public class GameManager : MonoBehaviour
     // PUBLIC METHODS
     public void OnStartGame()
     {
-
+        if (m_mainCharacter != null)
+        {
+            m_mainCharacter.OnStartGame();
+        }
     }
 
     public void OnResumeGame()
     {
-
+        if (m_mainCharacter != null)
+        {
+            m_mainCharacter.OnResumeGame();
+        }
     }
 
     public void OnPauseGame()
     {
-
+        if (m_mainCharacter != null)
+        {
+            m_mainCharacter.OnPauseGame();
+        }
     }
 
     public void OnStopGame()
     {
-
+        if (m_mainCharacter != null)
+        {
+            m_mainCharacter.OnStopGame();
+        }
     }
 
     // PRIVATE METHODS
---
+++ Unity/Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    // DEFINES
+    enum State
+    {
+        IDLE,
+        PLAYING,
+        STOP,
+        COUNT
+    }
+
+    // VARIABLES
+    State m_stage;
+
+    // PROPERTIES
+
+    // UNITY METHODS
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        switch(m_stage)
+        {
+            case State.IDLE:
+                break;
+            case State.PLAYING:
+                break;
+            case State.STOP:
+                break;
+            default:
+                break;
+        }
+    }
+
+    // PUBLIC METHODS
+    public void OnStartGame()
+    {
+        m_stage = State.PLAYING;
+    }
+
+    public void OnResumeGame()
+    {
+        m_stage = State.PLAYING;
+    }
+
+    public void OnPauseGame()
+    {
+        m_stage = State.STOP;
+    }
+
+    public void OnStopGame()
+    {
+        m_stage = State.IDLE;
+    }
+
+    // PRIVATE METHODS
+}
