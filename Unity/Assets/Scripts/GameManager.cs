c8a9229167d9e3356680833bf76e1070649e68d1
Unknown <thanhdungspk@yahoo.com.vn>
2018-04-15 17:00:44 +0700

    + Add simple menu

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // VARIABLES
+
+    // PROPERTIES
+
+    // UNITY METHODS
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    // PUBLIC METHODS
+    public void OnStartGame()
+    {
+
+    }
+
+    public void OnResumeGame()
+    {
+
+    }
+
+    public void OnPauseGame()
+    {
+
+    }
+
+    public void OnStopGame()
+    {
+
+    }
+
+    // PRIVATE METHODS
+}
