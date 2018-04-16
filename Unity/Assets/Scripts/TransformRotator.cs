354aff66a41a7107986f4861ae0e44aac967f343
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2018-04-16 19:19:27 +0700

    + Add TransformRotator

---
+++ Unity/Assets/Scripts/TransformRotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotator : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_anchor;
+    [SerializeField]
+    Vector3 m_axis;
+    [SerializeField]
+    float m_speed;
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        if (m_anchor != null)
+        {
+            transform.RotateAround(m_anchor.position, m_axis, m_speed);
+        }
+        else
+        {
+            transform.RotateAround(Vector3.zero, m_axis, m_speed);
+        }
+    }
+}
