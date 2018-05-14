084958f67cf212f7d06147c6d21b82e437b9eafd
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2018-05-14 17:51:31 +0700

    + Add scene1

---
+++ Unity/Assets/Scripts/Scene1/GameManager1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager1 : MonoBehaviour
+{
+    [SerializeField]
+    int score;
+    [SerializeField]
+    Text scoreText;
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            if (scoreText != null)
+                scoreText.text = score.ToString();
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Score++;
+        }
+    }
+}
