8ef309a73bbc8d5aa43cd2f7f47eb1deddef5d99
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2018-05-15 17:37:02 +0700

    + Load Scene1

---
+++ Unity/Assets/Scripts/UnitedGame/UnitedGameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnitedGameManager : MonoBehaviour
+{
+    public static UnitedGameManager Instance;
+
+    private const string GAME1_SCENE = "Game1";
+    private const string GAME2_SCENE = "Game2";
+
+    private int scene1Index;
+    private int scene2Index;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+    }
+
+    void Start()
+    {
+        SceneManager.LoadScene(GAME1_SCENE);
+    }
+
+    void Update()
+    {
+
+    }
+}
