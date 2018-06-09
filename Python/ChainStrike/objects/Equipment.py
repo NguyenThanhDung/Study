3696510d75a91a114446d4873a6a44592991f539
Unknown <thanhdungspk@yahoo.com.vn>
2018-06-09 18:04:52 +0700

    + Add Equipment class

---
+++ Python/ChainStrike/objects/Equipment.py
@@ -0,0 +1,43 @@
+import json
+
+class Equipment:
+
+    def __init__(self, id, atkPercent, atkPlus, defPercent, defPlus, pincerAtkPercent, pincerAtkPlus, hpPercent, hpPlus, crtRate, crtDmg, accuracy, resistance, set, starCount, level):
+        self.id = id
+        self.atkPercent = atkPercent
+        self.atkPlus = atkPlus
+        self.defPercent = defPercent
+        self.defPlus = defPlus
+        self.pincerAtkPercent = pincerAtkPercent
+        self.pincerAtkPlus = pincerAtkPlus
+        self.hpPercent = hpPercent
+        self.hpPlus = hpPlus
+        self.crtRate = crtRate
+        self.crtDmg = crtDmg
+        self.accuracy = accuracy
+        self.set = set
+        self.starCount = starCount
+        self.level = level
+
+    def ToString():
+        thisString = "Equipment #" + self.id + "\n"
+        thisString += "  ATK:" + "\n"
+        thisString += "    %:" + self.atkPercent + "\n"
+        thisString += "    +:" + self.atkPlus + "\n"
+        thisString += "  DEF:" + "\n"
+        thisString += "    %:" + self.defPercent + "\n"
+        thisString += "    +:" + self.defPlus + "\n"
+        thisString += "  Pincer ATK:" + "\n"
+        thisString += "    %:" + self.pincerAtkPercent + "\n"
+        thisString += "    +:" + self.pincerAtkPercent + "\n"
+        thisString += "  HP:" + "\n"
+        thisString += "    %:" + self.hpPercent + "\n"
+        thisString += "    +:" + self.hpPlus + "\n"
+        thisString += "  CRT Rate:" + self.crtRate + "\n"
+        thisString += "  CRT Dmg:" + self.crtDmg + "\n"
+        thisString += "  Accuracy:" + self.accuracy + "\n"
+        thisString += "  Resistance:" + self.resistance + "\n"
+        thisString += "  Set:" + self.set + "\n"
+        thisString += "  Star Count:" + self.starCount + "\n"
+        thisString += "  Level:" + self.level + "\n"
+        return thisString
