8aafb9e83c404c574a7130b48469c23dd923babf
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-04-18 14:29:59 +0700

    + Add touch controller

--- Unity/Vehicle/Assets/Scripts/VelocityController.cs
+++ Unity/Vehicle/Assets/Scripts/VelocityController.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum SteeringMethod
+{
+    Hold,
+    TouchBorder
+}
 
 public class VelocityController : MonoBehaviour
 {
     [SerializeField] Vehicle vehicle;
+    [SerializeField] Vector3 defaultFrontPointPosition;
+    [SerializeField] Vector3 defaultBackPointPosition;
     [SerializeField] Transform frontPoint;
     [SerializeField] Transform backPoint;
     [SerializeField] Transform destPoint;
     [SerializeField] float roadBorderDistance;
-
-    private const float SPEED_VELOC_RATIO = 0.1f;
-    private const float TIME_TO_REACH_DEST_POINT = 20f;
+    [SerializeField] float speedVelocityRatio;
+    [SerializeField] float timeToReachDestPoint;
+    [SerializeField] SteeringMethod steeringMethod;
+    [SerializeField] float steeringSensitivity;
 
     private float vehicleLength;
     private Vector3 speed;
@@ -21,37 +30,21 @@ public class VelocityController : MonoBehaviour
     void Start()
     {
         this.vehicleLength = (this.frontPoint.position - this.backPoint.position).magnitude;
+        Initialize();
     }
 
     void Update()
     {
         // Calculate speed vector
         Vector3 vehicleDirection = this.frontPoint.position - this.backPoint.position;
-        this.speed = vehicleDirection.normalized * this.vehicle.Speed * SPEED_VELOC_RATIO;
+        this.speed = vehicleDirection.normalized * this.vehicle.Speed * speedVelocityRatio;
 
         // Calculate steering vector
-        if (Input.GetMouseButtonDown(0))
-        {
-            this.lastTouchPosX = Input.mousePosition.x;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            float swipeDistance = Input.mousePosition.x - this.lastTouchPosX;
-            float steeringVolume = swipeDistance / Screen.width;
-            float steerVectorLength = steeringVolume * 10f;
-            Vector3 steerVector = this.frontPoint.position - this.backPoint.position;
-            steerVector.Normalize();
-            steerVector = Quaternion.Euler(0f, 90f, 0f) * steerVector;
-            this.steer = steerVector * steerVectorLength;
-        }
-        else
-        {
-            this.steer = Vector3.zero;
-        }
+        this.steer = GetSteeringVector(this.steeringMethod);
 
         // Calculate direction vector
         Vector3 direction = this.speed + this.steer;
-        direction = direction.normalized * speed.magnitude;
+        direction = direction.normalized * this.speed.magnitude;
 
         // Calculate steeringAngle
         float steeringAngle = Vector3.SignedAngle(direction, this.frontPoint.position - this.backPoint.position, Vector3.up);
@@ -61,11 +54,11 @@ public class VelocityController : MonoBehaviour
         this.destPoint.position = this.frontPoint.position + direction;
 
         // Move frontPoint to destPoint
-        float deltaDistance = direction.magnitude / TIME_TO_REACH_DEST_POINT * Time.deltaTime;
+        float deltaDistance = direction.magnitude / timeToReachDestPoint * Time.deltaTime;
         this.frontPoint.position += direction * deltaDistance;
-        if(this.frontPoint.position.x < -this.roadBorderDistance)
+        if (this.frontPoint.position.x < -this.roadBorderDistance)
             this.frontPoint.position = new Vector3(-this.roadBorderDistance, this.frontPoint.position.y, this.frontPoint.position.z);
-        if(this.frontPoint.position.x > this.roadBorderDistance)
+        if (this.frontPoint.position.x > this.roadBorderDistance)
             this.frontPoint.position = new Vector3(this.roadBorderDistance, this.frontPoint.position.y, this.frontPoint.position.z);
 
         // Move backPoint to frontPoint
@@ -73,16 +66,79 @@ public class VelocityController : MonoBehaviour
         float moveDistanceOfBackPoint = moveDirectionOfBackPoint.magnitude - this.vehicleLength;
         this.backPoint.position += moveDirectionOfBackPoint.normalized * moveDistanceOfBackPoint;
         this.backPoint.RotateAround(this.frontPoint.position, Vector3.up, -steeringAngle * 5f * Time.deltaTime);
-        if(this.backPoint.position.x < -this.roadBorderDistance)
+        if (this.backPoint.position.x < -this.roadBorderDistance)
             this.backPoint.position = new Vector3(-this.roadBorderDistance, this.backPoint.position.y, this.backPoint.position.z);
-        if(this.backPoint.position.x > this.roadBorderDistance)
+        if (this.backPoint.position.x > this.roadBorderDistance)
             this.backPoint.position = new Vector3(this.roadBorderDistance, this.backPoint.position.y, this.backPoint.position.z);
 
         // Move vehicle position to middle point of frontPoint and backPoint
         Vector3 moveDirectionOfVehicle = (this.frontPoint.position - this.backPoint.position) * 0.5f;
         this.transform.position = this.backPoint.position + moveDirectionOfVehicle;
 
+        // Rotate vehicle follow the direction of frontPoint and backPoint
         float angle = Vector3.SignedAngle(Vector3.forward, this.frontPoint.position - this.backPoint.position, Vector3.up);
         this.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    }
+
+    private void Initialize()
+    {
+        this.frontPoint.position = this.defaultFrontPointPosition;
+        this.backPoint.position = this.defaultBackPointPosition;
+        this.destPoint.position = this.defaultFrontPointPosition;
+        this.speed = Vector3.zero;
+        this.steer = Vector3.zero;
+    }
+
+    private Vector3 GetSteeringVector(SteeringMethod steeringMethod)
+    {
+        if (steeringMethod == SteeringMethod.Hold)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.lastTouchPosX = Input.mousePosition.x;
+                return this.steer;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                float swipeDistance = Input.mousePosition.x - this.lastTouchPosX;
+                float steeringVolume = swipeDistance / Screen.width;
+                float steerVectorLength = steeringVolume * this.steeringSensitivity;
+                Vector3 steerVector = this.frontPoint.position - this.backPoint.position;
+                steerVector.Normalize();
+                steerVector = Quaternion.Euler(0f, 90f, 0f) * steerVector;
+                return steerVector * steerVectorLength;
+            }
+            else
+            {
+                return Vector3.zero;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            {
+                float steeringVolume = 0f;
+                if (Input.GetMouseButton(0))
+                {
+                    float mouseX = Input.mousePosition.x;
+                    float screenMid = Screen.width / 2f;
+                    steeringVolume = (mouseX > screenMid) ? 0.3f : -0.3f;
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    steeringVolume = -0.3f;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    steeringVolume = 0.3f;
+                }
+                float steerVectorLength = steeringVolume * this.steeringSensitivity;
+                Vector3 steerVector = this.frontPoint.position - this.backPoint.position;
+                steerVector.Normalize();
+                steerVector = Quaternion.Euler(0f, 90f, 0f) * steerVector;
+                return steerVector * steerVectorLength;
+            }
+            return Vector3.zero;
+        }
     }
 }
