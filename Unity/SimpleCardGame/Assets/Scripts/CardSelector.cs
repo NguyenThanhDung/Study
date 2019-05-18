ce55e8b3fa8cccbf9fe13065ca8e9d187a3f8452
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2019-05-18 21:06:03 +0700

    + Ray cast to cards

---
+++ Unity/SimpleCardGame/Assets/Scripts/CardSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSelector : MonoBehaviour
+{
+    [SerializeField] LayerMask cardLayer;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit raycastHit;
+            if(Physics.Raycast(ray, out raycastHit, 100f, cardLayer))
+            {
+                Debug.Log(raycastHit.collider.name);
+            }
+        }
+    }
+}
