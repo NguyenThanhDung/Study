77379872c19a3de8a0aa6182c053773184ce0503
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2019-05-18 17:06:50 +0700

    + Add main menu

---
+++ Unity/SimpleCardGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public static MainMenu Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void StartGame()
+    {
+        if (GameEvents.OnGameStart != null)
+            GameEvents.OnGameStart.Invoke();
+    }
+}
