8fd58a6d806689bf6d8b1c2b73ea1cfc7c7a8930
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2019-05-18 22:54:57 +0700

    + Add cards to opponents

--- Unity/SimpleCardGame/Assets/Scripts/Opponent.cs
+++ Unity/SimpleCardGame/Assets/Scripts/Opponent.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Opponent : MonoBehaviour
 {
+    private List<Card> cards;
+
     void Start()
     {
+        this.cards = new List<Card>();
         GameEvents.OnDeliverCardToOpponent += GetCards;
     }
 
@@ -16,6 +19,20 @@ public class Opponent : MonoBehaviour
 
     private void GetCards(List<Card> cards)
     {
-        Debug.Log(cards.Count.ToString());
+        for (int i = 0; i < cards.Count; i++)
+            this.cards.Add(cards[i]);
+        AlignCards();
+    }
+
+    private void AlignCards()
+    {
+        float dist = 1.1f;
+        int count = this.cards.Count;
+        for(int i = 0; i < count; i++)
+        {
+            float x = i * dist - (count - 1) * 0.5f;
+            Vector3 position = new Vector3(x, 1f, 3f);
+            this.cards[i].MoveTo(position);
+        }
     }
 }
