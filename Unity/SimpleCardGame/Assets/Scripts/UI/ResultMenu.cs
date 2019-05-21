7ae84d00ebd4476d63b69dceb4c3f0924c0fb4a8
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-05-21 18:40:57 +0700

    + Add result menu

---
+++ Unity/SimpleCardGame/Assets/Scripts/UI/ResultMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultMenu : MonoBehaviour
+{
+    [SerializeField] GameObject display;
+
+    void OnEnable()
+    {
+        GameEvents.OnEndGame += OnEndGame;
+    }
+
+    void OnDisable()
+    {
+        GameEvents.OnEndGame -= OnEndGame;
+    }
+
+    public void Retry()
+    {
+
+    }
+
+    private void OnEndGame(PlayerType winner)
+    {
+        this.display.SetActive(true);
+    }
+}
