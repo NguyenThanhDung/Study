58a1e6d1ecd0e253376357130b8aeb15410b008e
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-05-23 18:48:22 +0700

    + Add card data

---
+++ Unity/SimpleCardGame/Assets/Scripts/CardData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardData : ScriptableObject
+{
+    public int AttackPoint;
+    public int DefendPoint;
+    public string avatarFileName;
+    public string description;
+}
