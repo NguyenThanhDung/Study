98972a209f697b00f4bb261f9831bab5b6b4c7c0
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-05-31 16:25:10 +0700

    - Align main menu

--- Unity/SimpleCardGame/Assets/Scripts/UI/MainMenu.cs
+++ Unity/SimpleCardGame/Assets/Scripts/UI/MainMenu.cs
@@ -6,8 +6,7 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject scorePanel;
-    [SerializeField] TMP_Text humanScoreText;
-    [SerializeField] TMP_Text computerScoreText;
+    [SerializeField] TMP_Text scoreText;
 
     void Start()
     {
@@ -23,8 +22,7 @@ public class MainMenu : MonoBehaviour
     private IEnumerator ShowScore()
     {
         yield return new WaitForSeconds(0.3f);
-        this.humanScoreText.text = Human.Instance.Score.ToString();
-        this.computerScoreText.text = Computer.Instance.Score.ToString();
+        this.scoreText.text = Human.Instance.Score.ToString() + "\n" + Computer.Instance.Score.ToString();
         this.scorePanel.SetActive(true);
     }
 }
