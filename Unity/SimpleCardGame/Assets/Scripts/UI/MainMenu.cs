5ae1f5a7918cb091a236b2bbf7a89afb663027e9
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-05-31 14:39:34 +0700

    - Show score on main menu

--- Unity/SimpleCardGame/Assets/Scripts/UI/MainMenu.cs
+++ Unity/SimpleCardGame/Assets/Scripts/UI/MainMenu.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject scorePanel;
+    [SerializeField] TMP_Text humanScoreText;
+    [SerializeField] TMP_Text computerScoreText;
+
+    void Start()
+    {
+        StartCoroutine(ShowScore());
+    }
+
     public void StartGame()
     {
         if (GameEvents.OnStartGame != null)
             GameEvents.OnStartGame.Invoke();
+    }
+
+    private IEnumerator ShowScore()
+    {
+        yield return new WaitForSeconds(0.3f);
+        this.humanScoreText.text = Human.Instance.Score.ToString();
+        this.computerScoreText.text = Computer.Instance.Score.ToString();
+        this.scorePanel.SetActive(true);
     }
 }
