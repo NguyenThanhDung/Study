b234eeaf1b08d57435bce03a07568589b9a55d67
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-08-07 11:06:37 +0700

    + Add buttons to set current colors

---
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/ColorSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSetter : MonoBehaviour
+{
+    [SerializeField] Image targetImage;
+
+    public void OnSetColor()
+    {
+        Image thisImage = this.GetComponent<Image>();
+        Color thisColor = thisImage.color;
+        targetImage.color = thisColor;
+    }
+}
