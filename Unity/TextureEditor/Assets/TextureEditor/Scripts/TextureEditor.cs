cf08d6f564e85fb4f1d14a95ca881a916fb324fe
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-08-07 11:56:16 +0700

    + Change cube color

---
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/TextureEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextureEditor : MonoBehaviour
+{
+    [SerializeField] Image currentImage;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject.layer == LayerMask.NameToLayer("ColorEditable"))
+                {
+                    MeshRenderer renderer = hit.collider.GetComponent<MeshRenderer>();
+                    renderer.materials[0].SetColor("_Color", currentImage.color);
+                }
+            }
+        }
+    }
+}
