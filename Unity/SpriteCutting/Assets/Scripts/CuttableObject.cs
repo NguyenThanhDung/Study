91f9e37b1cc56314cafd5c202ceb63c3574812c9
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-09-12 11:23:58 +0700

    + Add CuttableObject

---
+++ Unity/SpriteCutting/Assets/Scripts/CuttableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttableObject : MonoBehaviour
+{
+    [SerializeField] LayerMask cuttableLayer;
+
+    void Update()
+    {
+        if(!Input.GetMouseButton(0))
+            return;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, this.cuttableLayer))
+        {
+            Debug.Log(hit.textureCoord);
+        }
+    }
+}
