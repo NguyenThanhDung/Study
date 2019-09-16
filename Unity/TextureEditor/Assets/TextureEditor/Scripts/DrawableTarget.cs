cceedb87b13d3c258a26af033fdebfb60427048b
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-09-16 15:59:20 +0700

    + Save texture to user data

--- Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableTarget.cs
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableTarget.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class DrawableTarget : MonoBehaviour
 {
+    [SerializeField] Texture2D templateTexture;
+
     void Start()
     {
-        MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
-        Texture2D texture = (Texture2D)meshRenderer.material.mainTexture;
-        byte[] bytes = texture.EncodeToPNG();
+        // MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+        // Texture2D texture = (Texture2D)meshRenderer.material.mainTexture;
+        // byte[] bytes = texture.EncodeToPNG();
+        byte[] bytes = templateTexture.EncodeToPNG();
         File.WriteAllBytes(Application.persistentDataPath + "/DrawableTarget.png", bytes);
     }
 }
