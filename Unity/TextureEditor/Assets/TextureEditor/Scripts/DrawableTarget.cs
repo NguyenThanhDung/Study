a490629389e4f0f73771126c58780b0f5f988924
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-09-18 16:54:58 +0700

    + Add DrawZone prefab

--- Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableTarget.cs
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableTarget.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 
 public class DrawableTarget : MonoBehaviour
 {
+    [SerializeField] GameObject display;
     [SerializeField] Texture2D texture;
 
     void Start()
     {
         byte[] bytes = texture.EncodeToPNG();
-
         Texture2D newTexture = new Texture2D(2, 2);
         newTexture.LoadImage(bytes);
-        Transform child = this.transform.GetChild(0);
-        MeshRenderer meshRenderer = child.gameObject.GetComponent<MeshRenderer>();
+        MeshRenderer meshRenderer = this.display.GetComponent<MeshRenderer>();
         meshRenderer.material.mainTexture = newTexture;
     }
 
     public void SaveTextureToFile()
     {
-        Transform child = this.transform.GetChild(0);
-        MeshRenderer meshRenderer = child.gameObject.GetComponent<MeshRenderer>();
+        MeshRenderer meshRenderer = this.display.GetComponent<MeshRenderer>();
         Texture2D texture = (Texture2D)meshRenderer.material.mainTexture;
         byte[] bytes = texture.EncodeToPNG();
         File.WriteAllBytes(Application.persistentDataPath + "/DrawableTarget.png", bytes);
