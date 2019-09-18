ada4c982b8506e7e04a239ec6e738b190ba85528
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-09-18 17:29:13 +0700

    + Load drawable object and attach to draw zone

---
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawZone : MonoBehaviour
+{
+    [SerializeField] GameObject drawableObjectPrefab;
+
+    public void LoadDrawableObject()
+    {
+        GameObject drawableObject = Instantiate(drawableObjectPrefab, this.transform);
+    }
+}
--- Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableObject.cs
+++ Unity/TextureEditor/Assets/TextureEditor/Scripts/DrawableObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class DrawableTarget : MonoBehaviour
+public class DrawableObject : MonoBehaviour
 {
     [SerializeField] GameObject display;
     [SerializeField] Texture2D texture;
