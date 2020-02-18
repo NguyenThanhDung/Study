b24629c0e48da8f8cdef442c8bf25c042773192e
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2020-02-18 21:53:48 +0700

    + Array exercise

---
+++ DataStructureAndAlgorithms/Array/KthSmallestElement/CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// https://practice.geeksforgeeks.org/problems/kth-smallest-element/0
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = Console.ReadLine();
+            int numberOfTestCases = Convert.ToInt32(val);
+            Console.WriteLine(numberOfTestCases);
+        }
+    }
+}
