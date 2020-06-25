1c1b2a41f17f7df6001f4af5288fb544b3290c1d
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-06-25 18:41:25 +0700

    + Get Date Time from Game Manager

---
+++ Unity/Countdown/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] private int year;
+    [SerializeField] private int month;
+    [SerializeField] private int day;
+    [SerializeField] private int hour;
+    [SerializeField] private int minute;
+
+    public DateTime ReleaseDate { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        this.ReleaseDate = new DateTime(this.year, this.month, this.day, this.hour, this.minute, 0);
+    }
+}
---
+++ Unity/Countdown/Assets/Scripts/RemaingTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemaingTime : MonoBehaviour
+{
+    void Start()
+    {
+        var text = GetComponent<Text>();
+        text.text = GameManager.Instance.ReleaseDate.ToShortDateString();
+    }
+}
