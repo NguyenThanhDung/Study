0d0ffcd12b395ce8d001dceae5a008a64970274d
Nguyen Thanh Dung <thanhdungspk@yahoo.com.vn>
2020-07-26 13:08:16 +0700

    + Calculate correct size

--- Python/FileManager/FileManager.py
+++ Python/FileManager/FileManager.py
@@ -1,5 +1,16 @@
 import os
 
+# https://stackoverflow.com/a/1392549
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
 if __name__ == "__main__":
     
     currentDir = os.getcwd()
@@ -7,5 +18,5 @@ if __name__ == "__main__":
 
     subDirsAndFiles = os.listdir()
     for dirOrFile in subDirsAndFiles:
-        statInfo = os.stat(dirOrFile)
-        print(dirOrFile + " " + str(statInfo.st_size))
+        size = get_size(dirOrFile)
+        print(dirOrFile + " " + str(size))
