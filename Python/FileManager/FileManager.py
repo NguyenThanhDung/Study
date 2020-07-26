ce0768cb6a839acab84a7ccb166856c6d0bd8d78
Nguyen Thanh Dung <thanhdungspk@yahoo.com.vn>
2020-07-26 12:27:26 +0700

    + List all directories and files in current directory

--- Python/FileManager/FileManager.py
+++ Python/FileManager/FileManager.py
@@ -1,5 +1,10 @@
 import os
 
 if __name__ == "__main__":
+    
     currentDir = os.getcwd()
     print(currentDir)
+
+    subDirsAndFiles = os.listdir()
+    for dirOrFile in subDirsAndFiles:
+        print(dirOrFile)
